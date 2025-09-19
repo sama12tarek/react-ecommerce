81e002153fc10d42703513970b0a89a425ac0d43
sama12tarek <148053813+sama12tarek@users.noreply.github.com>
2025-09-20 00:50:09 +0300

    Update allProducts.tsx

--- src/app/_components/allProducts/allProducts.tsx
+++ src/app/_components/allProducts/allProducts.tsx
@@ -1,13 +1,19 @@
 
 import getProduct from '@/lib/api/products.api';
 import SingleProduct from '@/app/singleProduct/singleProduct';
+type Product = {
+  id: string;
+  title: string;
+  price: number;
+  image: string;
+};
 export default async function AllProducts () {
   const data=await getProduct()
   return (
     <>
         <div className='container my-12 w-[80%] mx-auto'>
             <div className='flex flex-wrap gap-4 p-4 justify-evenly'>
-              {data.map((product) => (
+              {data.map((product:product) => (
                 <SingleProduct key={product.id} product={product} />
               ))}
             </div>
