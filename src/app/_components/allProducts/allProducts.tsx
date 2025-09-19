f5f3af3a6624dff2d0296730b3f93bf8c006ed57
sama12tarek <148053813+sama12tarek@users.noreply.github.com>
2025-09-20 01:18:23 +0300

    Update allProducts.tsx

--- src/app/_components/allProducts/allProducts.tsx
+++ src/app/_components/allProducts/allProducts.tsx
@@ -11,12 +11,7 @@ export interface ProductType {
   slug: string
   description: string
   quantity: number
-  price: number
-  imageCover: string
-  category: Category
-  brand: Brand
   ratingsAverage: number
-  createdAt: string
   updatedAt: string
   id: string
   
