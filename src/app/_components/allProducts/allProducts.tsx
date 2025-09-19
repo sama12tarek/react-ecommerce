016696a4d187163158f4a0bec9ec2d45dbe6752e
sama12tarek <148053813+sama12tarek@users.noreply.github.com>
2025-09-20 00:44:00 +0300

    Update allProducts.tsx

--- src/app/_components/allProducts/allProducts.tsx
+++ src/app/_components/allProducts/allProducts.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import getProduct from '@/lib/api/products.api';
 import SingleProduct from '@/app/singleProduct/singleProduct';
 export default async function AllProducts () {
-  let data=await getProduct()
+  const data=await getProduct()
   return (
     <>
         <div className='container my-12 w-[80%] mx-auto'>
