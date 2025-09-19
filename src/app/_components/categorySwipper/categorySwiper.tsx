23c1b57e6690e53b3a51bd45ee2c4efafdfa70ae
sama12tarek <148053813+sama12tarek@users.noreply.github.com>
2025-09-20 01:26:37 +0300

    Update categorySwiper.tsx

--- src/app/_components/categorySwipper/categorySwiper.tsx
+++ src/app/_components/categorySwipper/categorySwiper.tsx
@@ -1,40 +1,45 @@
 'use client';
 
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Autoplay } from 'swiper/modules';
+import Image from 'next/image';
 
-export default function CategorySwiper({ data }) {
+export type CategoryType = {
+  _id: string;
+  name: string;
+  image: string;
+  // أي خصائص إضافية قد تكون لديك
+};
+// هنا حددنا النوع بدقة أكثر
+interface CategorySwiperProps {
+  data: CategoryType[];
+}
+
+export default function CategorySwiper({ data }: CategorySwiperProps) {
   return (
     <div className="mx-auto w-[80%]">
       <h1 className='text-2xl font-bold text-pink-500 my-4'> category slider</h1>
       <Swiper
         spaceBetween={0}
         slidesPerView={7}
-  
         modules={[Autoplay]}
         autoplay={{ delay: 2000 }}
         loop
       >
-        {data.map((category) => (
+        {data.map((category: CategoryType) => (
           <SwiperSlide key={category._id}>
-          
-                <img
-                  src={category.image}
-                  alt={category.name}
-                
-                  className="object-cover w-full h-[150px]"
-                />
-            
-              <p className="mt-2 text-center text-sm font-medium">{category.name}</p>
-          
+            <Image
+              src={category.image}
+              alt={category.name}
+              width={200}
+              height={200}
+              className="object-cover w-full h-[150px]"
+            />
+            <p className="mt-2 text-center text-sm font-medium">{category.name}</p>
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
 }
-
-
-
