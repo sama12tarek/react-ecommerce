bab48947f4d398cc385231ab0805b82137a4f293
sama12tarek <148053813+sama12tarek@users.noreply.github.com>
2025-09-20 08:30:29 +0300

    Update page.tsx

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,6 +1,11 @@
 import selectedsProduct from '@/lib/api/selectedProduct';
 import React from 'react';
 import Details from '@/app/_components/details/details';
+type Params = {
+  params: {
+    id: string;
+  };
+};
 export default async function ProductDetails({ params }) {
   const { id } = await params;
 
