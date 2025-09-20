a04a80c75161e0f19cce1d971ac8fd4ae4e6e7cf
search <samaatarek12z@gmail.com>
2025-09-20 09:41:23 +0300

    حفظ مؤقت لتعديلات محلية قبل rebase

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,26 +1,21 @@
 import selectedsProduct from '@/lib/api/selectedProduct';
 import React from 'react';
 import Details from '@/app/_components/details/details';
-import relatedProducts from '@/productCategoryAction/relatedProducts';
-import SingleProduct from '@/app/singleProduct/singleProduct';
-import { ProductType } from '@/types/product.type';
 
-export default async function ProductDetails({ params }:{params:Promise<{id:string}>}) {
-  const { id } = await params;
+type Params = {
+  params: {
+    id: string;
+  };
+};
 
-  const data= await selectedsProduct(id);
-if(!data){
-  return<h1>thier isnt data</h1>
-}
-const dataCategory=await relatedProducts(data.category._id)
+export default async function ProductDetails({ params }: Params) {
+  const { id } = params;
+
+  const data = await selectedsProduct(id);
+
   return (
-  <>
-<Details data={data}/>
-  <div className='flex flex-wrap gap-4 p-4 justify-evenly'>
-        {dataCategory.data.map((product:ProductType) => (
-          <SingleProduct key={product.id} product={product} />
-        ))}
-      </div>
-  </>
+    <>
+      <Details data={data} />
+    </>
   );
 }
